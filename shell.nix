32879131b770ee47e769c3e56af66eb4ad2e771a
sd <ze>
2020-02-09 19:13:48 +1100

    nix-shell sans avoir besoin de télécharger

--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
 # https://discourse.nixos.org/t/how-do-i-build-a-nix-shell-which-depends-on-some-unstable-packages/928/2
 let
 # for sedlex_2
-  unstable =
-# <nixos-unstable> {} ;
-     import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+  unstable = import <nixos-unstable> {} ;
+     # import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
 in
 { nixpkgs ? import <nixpkgs> {} }:
 with nixpkgs; mkShell {
